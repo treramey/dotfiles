98f71fe3c5106b17248eff326c4aad1459f5e315
Trevor Ramey <hello@trevors.email>
2024-12-07 14:39:51 -0600

    WIP

---
+++ home/dot_config/zsh/scripts/aliais.zsh
@@ -0,0 +1,49 @@
+#!/usr/bin/env zsh
+
+# custom
+alias ws="cd $WORKSPACE"
+
+# ls
+alias ls='ls --color=auto'
+alias l='ls -lAFh'
+alias la='ls -lAFh'
+alias lr='ls -tRFh'
+alias lt='ls -ltFh'
+alias ll='ls -l'
+
+# grep
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# nvim
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+
+# rider
+function rider() {
+    # Use provided directory or current directory if none provided
+    local dir="${1:-.}"
+    
+    # Find the single Rider application
+    local rider_app=$(find /Applications -maxdepth 1 -name "Rider*.app" | head -n 1)
+    if [ -z "$rider_app" ]; then
+        echo "No Rider installation found."
+        return 1
+    fi
+
+    # Find the single .sln file in the specified directory
+    local sln_file=$(find "$dir" -name "*.sln" | head -n 1)
+    if [ -z "$sln_file" ]; then
+        echo "No .sln file found in the specified directory or its subdirectories."
+        return 1
+    fi
+
+    # Open the .sln file with Rider
+    open -na "$rider_app" --args "$sln_file"
+}
+
+alias zj=zellij
+alias lg=lazygit
+
---
+++ home/dot_config/zsh/scripts/completion.zsh
@@ -0,0 +1,78 @@
+#!/usr/bin/env zsh
+
+fpath=($ZDOTDIR/plugins/zsh-completions/src $fpath)
+
+# should be called before completions
+zmodload zsh/complist
+
+# Use hjlk in menu selection (during completion)
+# Doesn't work well with interactive mode
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+
+autoload -Uz compinit && compinit -i
+ # With hidden files
+_comp_options+=(globdots)
+
+# setopt GLOB_COMPLETE      # Show autocompletion menu with globs
+setopt menu_complete        # Automatically highlight first element of completion menu
+setopt auto_list            # Automatically list choices on ambiguous completion.
+setopt complete_in_word     # Complete from both ends of a word.
+
+# +---------+
+# | zstyles |
+# +---------+
+
+# Ztyle pattern
+# :completion:<function>:<completer>:<command>:<argument>:<tag>
+
+# Define completers
+zstyle ':completion:*' completer _extensions _complete _approximate
+
+# Use cache for commands using cache
+zstyle ':completion:*' use-cache on
+# zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+zstyle ':completion:*' cache-path "$ZDOTDIR/.zcompcache"
+# Complete the alias when _expand_alias is used as a function
+zstyle ':completion:*' complete true
+
+zle -C alias-expension complete-word _generic
+bindkey '^Xa' alias-expension
+zstyle ':completion:alias-expension:*' completer _expand_alias
+
+# Use cache for commands which use it
+
+# Allow you to select in a menu
+zstyle ':completion:*' menu select
+
+# Autocomplete options for cd instead of directory stack
+zstyle ':completion:*' complete-options true
+
+zstyle ':completion:*' file-sort modification
+
+
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
+zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
+# zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+# Colors for files and directory
+zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+
+# Only display some tags for the command cd
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+# zstyle ':completion:*:complete:git:argument-1:' tag-order !aliases
+
+# Required for completion to be in good groups (named after the tags)
+zstyle ':completion:*' group-name ''
+
+zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
+
+# See ZSHCOMPWID "completion matching control"
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+zstyle ':completion:*' keep-prefix true
+
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
