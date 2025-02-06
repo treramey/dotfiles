06de155d94b220cc63ef0c5a82e74c53e0b2af0b
Trevor Ramey <hello@trevors.email>
2025-02-06 00:46:08 -0600

    WIP

---
+++ home/dot_config/tmux/plugins/tmux2k/scripts/cpu.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+export LC_ALL=en_US.UTF-8
+
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$current_dir"/utils.sh
+
+get_percent() {
+    case $(uname -s) in
+    Linux)
+        percent=$(LC_NUMERIC=en_US.UTF-8 top -bn2 -d 0.01 | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+        normalize_padding "$percent"
+        ;;
+
+    Darwin)
+        cpuvalue=$(ps -A -o %cpu | awk -F. '{s+=$1} END {print s}')
+        cpucores=$(sysctl -n hw.logicalcpu)
+        cpuusage=$((cpuvalue / cpucores))
+        percent="$cpuusage"
+        echo "$percent"
+        ;;
+
+    CYGWIN* | MINGW32* | MSYS* | MINGW*) ;; # TODO - windows compatibility
+    esac
+}
+
+get_load() {
+    case $(uname -s) in
+    Linux | Darwin)
+        loadavg=$(uptime | awk -F'[a-z]:' '{ print $2}' | sed 's/,//g')
+        echo "$loadavg"
+        ;;
+
+    CYGWIN* | MINGW32* | MSYS* | MINGW*) ;; # TODO - windows compatibility
+    esac
+}
+
+main() {
+    RATE=$(get_tmux_option "@tmux2k-refresh-rate" 5)
+    cpu_percent=$(get_percent)
+    if [ "$cpu_percent" -gt 50 ]; then
+      echo "#[fg="red"] $cpu_percent%"
+    elif [ "$cpu_percent" -gt 10 ]; then
+      echo "#[fg="orange"] $cpu_percent%"
+    else
+      # echo "#[fg="green"] $cpu_percent%"
+      echo " $cpu_percent%"
+    fi
+    sleep "$RATE"
+}
+
+main
