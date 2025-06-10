48ff930005d267a7fd22027b970ef4e03445d31f
Trevor Ramey <hello@trevors.email>
2025-06-10 11:06:43 -0500

    wip

--- home/dot_config/karabiner/utils.ts
+++ home/dot_config/karabiner/utils.ts
@@ -165,17 +165,5 @@ export function open(what: string): LayerCommand {
  * Shortcut for "Open an app" command (of which there are a bunch)
  */
 export function app(name: string): LayerCommand {
-	return {
-		to: [
-			{
-				shell_command: `open -a '${name}.app'`,
-				hold_down_milliseconds: 800,
-			},
-			{
-				key_code: "m",
-				modifiers: ["left_option"],
-			},
-		],
-		description: `Open ${name} and send Option+M`,
-	};
+	return open(`-a '${name}.app'`);
 }
